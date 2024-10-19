19a4274aef1ba35ac7287e0e297a337e1a939d9f
Juan Vásquez <juan@ombulabs.com>
2024-10-19 12:34:52 -0600

    Add Entries list (#1)
    * Add Entries list
    
    - [x] Request entries from the server
    - [x] Display entries in a list
    - [x] Limit the number of entries displayed (today only)
    - [x] Move the API call to a separate function
    - [x] Move Types to a separate file
    
    * Create useEntries hook
    
    I'm not pretty sure if this is the best way to implement this hook, but it's working for now.
    
    * Add entry details in the Entries list
    
    * Add entryDecorator
    
    * Update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Noko Changelog
 
+## [Entries] - 2024-10-05
+
+- Add the ability to list entries.
+- Add NokoId input configuration.
+
 ## [Initial Version] - 2024-09-19
 
 - Add the ability to create new entries in Noko.
---
+++ src/types.ts
@@ -0,0 +1,38 @@
+interface IPreferences {
+  personalAccessToken: string;
+  userId: number;
+}
+
+type Project = {
+  id: string;
+  name: string;
+  color: string;
+};
+
+type User = {
+  id: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  profile_image_url: string;
+};
+
+type Tag = {
+  id: string;
+  name: string;
+  formatted_name: string;
+};
+
+type Entry = {
+  id: string;
+  date: string;
+  billable: boolean;
+  minutes: number;
+  formatted_minutes: string;
+  description: string;
+  user: User;
+  tags: Tag[];
+  project: Project;
+};
+
+export type { Entry, IPreferences };
