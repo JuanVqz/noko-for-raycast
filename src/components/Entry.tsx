fcf1fe80ada535757fe1131268206949aad44dee
Juan Vásquez <juan@ombulabs.com>
2024-10-27 11:02:27 -0600

    Create Entry component (#3)
    - [x] improve require hooks and components by index.tsx each

---
+++ src/components/Entry.tsx
@@ -0,0 +1,49 @@
+import { Icon, List } from '@raycast/api';
+import { Entry as TEntry } from "../types";
+
+const Entry: React.FC<{entry: TEntry}> = ({ entry }) => {
+  return (
+    <List.Item
+      key={entry.id}
+      title={entry.project.name}
+      subtitle={entry.formatted_minutes}
+      icon={{
+        source: Icon.CircleFilled,
+        tintColor: entry.project.color
+      }}
+      detail={
+        <List.Item.Detail
+          metadata={
+            <List.Item.Detail.Metadata>
+              <List.Item.Detail.Metadata.Label title="Description" />
+              <List.Item.Detail.Metadata.Label title={entry.description} />
+              <List.Item.Detail.Metadata.Separator />
+              <List.Item.Detail.Metadata.Label title="Tags" text={entry.tags.map((tag) => tag.formatted_name).join(', ')} />
+              <List.Item.Detail.Metadata.Separator />
+              <List.Item.Detail.Metadata.Label title="Project" text={entry.project.name} />
+              <List.Item.Detail.Metadata.Separator />
+              <List.Item.Detail.Metadata.Label title="Time" text={entry.formatted_minutes} />
+              <List.Item.Detail.Metadata.Separator />
+              <List.Item.Detail.Metadata.Label title="Date" text={entry.date} />
+              <List.Item.Detail.Metadata.Separator />
+              <List.Item.Detail.Metadata.Label title="Billable" text={entry.billable ? 'Yes' : 'No'} />
+              <List.Item.Detail.Metadata.Separator />
+              <List.Item.Detail.Metadata.Label title="User" icon={entry.user.profile_image_url} text={`${entry.user.first_name} ${entry.user.last_name} <${entry.user.email}>`} />
+              <List.Item.Detail.Metadata.Separator />
+              {entry.approved_by && (
+                <>
+                  <List.Item.Detail.Metadata.Label title="Approved By" icon={entry.approved_by.profile_image_url} text={`${entry.approved_by.first_name} ${entry.approved_by.last_name} <${entry.approved_by.email}>`} />
+                  <List.Item.Detail.Metadata.Separator />
+                  <List.Item.Detail.Metadata.Label title="Approved At" text={entry.approved_at} />
+                  <List.Item.Detail.Metadata.Separator />
+                </>
+              )}
+            </List.Item.Detail.Metadata>
+          }
+        />
+      }
+    />
+  );
+}
+
+export default Entry;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import { Icon, List } from '@raycast/api';
+import { List } from '@raycast/api';
 
-import  { Entry, Filter } from "./types";
-import { useEntries } from './hooks/useEntries';
+import  { Entry as TEntry, Filter } from "./types";
+
+import { useEntries } from './hooks';
+
+import { Entry } from './components';
 
 export default function Command() {
-
   const { isLoading, filter, entries, setState} = useEntries();
 
   const handleChangeFilter = (value: Filter) => {
@@ -25,48 +27,10 @@ export default function Command() {
         </List.Dropdown>
       }
       isLoading={isLoading}
-      isShowingDetail>
-      {entries.map((entry: Entry) => (
-        <List.Item
-          key={entry.id}
-          title={entry.project.name}
-          subtitle={entry.formatted_minutes}
-          icon={{
-            source: Icon.CircleFilled,
-            tintColor: entry.project.color
-          }}
-          detail={
-            <List.Item.Detail
-              metadata={
-                <List.Item.Detail.Metadata>
-                  <List.Item.Detail.Metadata.Label title="Description" />
-                  <List.Item.Detail.Metadata.Label title={entry.description} />
-                  <List.Item.Detail.Metadata.Separator />
-                  <List.Item.Detail.Metadata.Label title="Tags" text={entry.tags.map((tag) => tag.formatted_name).join(', ')} />
-                  <List.Item.Detail.Metadata.Separator />
-                  <List.Item.Detail.Metadata.Label title="Project" text={entry.project.name} />
-                  <List.Item.Detail.Metadata.Separator />
-                  <List.Item.Detail.Metadata.Label title="Time" text={entry.formatted_minutes} />
-                  <List.Item.Detail.Metadata.Separator />
-                  <List.Item.Detail.Metadata.Label title="Date" text={entry.date} />
-                  <List.Item.Detail.Metadata.Separator />
-                  <List.Item.Detail.Metadata.Label title="Billable" text={entry.billable ? 'Yes' : 'No'} />
-                  <List.Item.Detail.Metadata.Separator />
-                  <List.Item.Detail.Metadata.Label title="User" icon={entry.user.profile_image_url} text={`${entry.user.first_name} ${entry.user.last_name} <${entry.user.email}>`} />
-                  <List.Item.Detail.Metadata.Separator />
-                  {entry.approved_by && (
-                    <>
-                      <List.Item.Detail.Metadata.Label title="Approved By" icon={entry.approved_by.profile_image_url} text={`${entry.approved_by.first_name} ${entry.approved_by.last_name} <${entry.approved_by.email}>`} />
-                      <List.Item.Detail.Metadata.Separator />
-                      <List.Item.Detail.Metadata.Label title="Approved At" text={entry.approved_at} />
-                      <List.Item.Detail.Metadata.Separator />
-                    </>
-                  )}
-                </List.Item.Detail.Metadata>
-              }
-            />
-          }
-        />
+      isShowingDetail
+    >
+      {entries.map((entry: TEntry) => (
+        <Entry key={entry.id} entry={entry} />
       ))}
     </List>
   );
